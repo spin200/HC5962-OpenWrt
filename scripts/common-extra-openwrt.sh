a0887876b23ad3cbdb9b51f8a25f342de02b0f4f
spin200 <67430783+spin200@users.noreply.github.com>
2023-11-09 22:34:43 +0800

    Update common-extra-openwrt.sh

--- scripts/common-extra-openwrt.sh
+++ scripts/common-extra-openwrt.sh
@@ -1,7 +1,8 @@
 # 增加自定义配置
-svn co https://github.com/danxiaonuo/AutoBuild-OpenWrt/trunk/server/default-settings package/danxiaonuo/default-settings
+#svn co https://github.com/danxiaonuo/AutoBuild-OpenWrt/trunk/server/default-settings package/danxiaonuo/default-settings
+svn co https://github.com/spin200/HC5962-OpenWrt/tree/master/server/default-settings package/danxiaonuo/default-settings
 # 增加IPV6
-curl -fsSL https://raw.githubusercontent.com/danxiaonuo/AutoBuild-OpenWrt/master/server/etc/99-ipv6 > package/base-files/files/etc/hotplug.d/99-ipv6
+curl -fsSL https://github.com/spin200/HC5962-OpenWrt/blob/master/server/etc/99-ipv6 > package/base-files/files/etc/hotplug.d/99-ipv6
 sed -i '/exit 0/i\mv /etc/hotplug.d/99-ipv6 /etc/hotplug.d/iface/99-ipv6' package/danxiaonuo/default-settings/files/zzz-default-settings
 sed -i '/99-ipv6/a\chmod u+x /etc/hotplug.d/iface/99-ipv6' package/danxiaonuo/default-settings/files/zzz-default-settings
 # 修改smartdns配置
