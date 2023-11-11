573285e203ad56c2b054da74e992858772bced34
spin100 <55934822+spin100@users.noreply.github.com>
2023-11-11 13:43:29 +0800

    Update common-extra-openwrt.sh

--- scripts/common-extra-openwrt.sh
+++ scripts/common-extra-openwrt.sh
@@ -1,8 +1,7 @@
 # 增加自定义配置
-#svn co https://github.com/danxiaonuo/AutoBuild-OpenWrt/trunk/server/default-settings package/danxiaonuo/default-settings
-svn co https://github.com/spin200/HC5962-OpenWrt/tree/master/server/default-settings package/danxiaonuo/default-settings
+svn co https://github.com/danxiaonuo/AutoBuild-OpenWrt/trunk/server/default-settings package/danxiaonuo/default-settings
 # 增加IPV6
-curl -fsSL https://raw.githubusercontent.com/spin200/HC5962-OpenWrt/master/server/etc/99-ipv6 > package/base-files/files/etc/hotplug.d/99-ipv6
+curl -fsSL https://raw.githubusercontent.com/danxiaonuo/AutoBuild-OpenWrt/master/server/etc/99-ipv6 > package/base-files/files/etc/hotplug.d/99-ipv6
 sed -i '/exit 0/i\mv /etc/hotplug.d/99-ipv6 /etc/hotplug.d/iface/99-ipv6' package/danxiaonuo/default-settings/files/zzz-default-settings
 sed -i '/99-ipv6/a\chmod u+x /etc/hotplug.d/iface/99-ipv6' package/danxiaonuo/default-settings/files/zzz-default-settings
 # 修改smartdns配置
@@ -10,6 +9,6 @@ curl -fsSL https://raw.githubusercontent.com/danxiaonuo/AutoBuild-OpenWrt/master
 curl -fsSL https://raw.githubusercontent.com/danxiaonuo/AutoBuild-OpenWrt/master/server/smartdns/smartdns.conf > package/base-files/files/etc/smartdns.conf
 sed -i '/exit 0/i\# 修改smartdns配置\nmv /etc/newsmartdns /etc/config/smartdns\nmv /etc/smartdns.conf /var/etc/smartdns/smartdns.conf' package/danxiaonuo/default-settings/files/zzz-default-settings
 # 修改系统欢迎词
-curl -fsSL https://raw.githubusercontent.com/spin200/HC5962-OpenWrt/master/server/etc/bannera > package/base-files/files/etc/banner
+curl -fsSL https://raw.githubusercontent.com/danxiaonuo/AutoBuild-OpenWrt/master/server/etc/banner > package/base-files/files/etc/banner
 # 修改系统内核参数
 curl -fsSL https://raw.githubusercontent.com/danxiaonuo/AutoBuild-OpenWrt/master/server/etc/sysctl.conf > package/base-files/files/etc/sysctl.conf
