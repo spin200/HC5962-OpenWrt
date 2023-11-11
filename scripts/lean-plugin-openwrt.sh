02d94f8df27a2740882cb5cc2dbb35916f2db3d6
spin200 <67430783+spin200@users.noreply.github.com>
2023-11-11 15:52:13 +0800

    Update lean-plugin-openwrt.sh

--- scripts/lean-plugin-openwrt.sh
+++ scripts/lean-plugin-openwrt.sh
@@ -1,5 +1,6 @@
 # 增加 lienol passwall
 echo "src-git lienol https://github.com/chenshuo890/lienol-openwrt-package.git" >> feeds.conf.default
+echo "src-git kiddin9 https://github.com/kiddin9/openwrt-packages" >> feeds.conf.default
 
 # 更改默认主题为Argon
 rm -rf package/lean/luci-theme-argon
@@ -15,7 +16,7 @@ svn co https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t/luci-app-
 #svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-smartdns package/danxiaonuo/luci-app-smartdns
 
 # 复杂的AdGuardHome的openwrt的luci界面
-git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/danxiaonuo/luci-app-adguardhome
+#git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/danxiaonuo/luci-app-adguardhome
 
 # Server酱
 #git clone https://github.com/tty228/luci-app-serverchan.git package/danxiaonuo/luci-app-serverchan
